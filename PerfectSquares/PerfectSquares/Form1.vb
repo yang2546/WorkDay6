b2de4d98e016dacacdaf477b4fd2d18fc8faf0a5
idsc3101 <idsc3101@gmail.com>
2013-10-09 15:14:37 -0500

    solved both problems

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,17 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Dim counter, X As Integer
+
+        counter = 1
+        Do While (counter < 11)
+
+            X = counter * counter
+            lstResult.Items.Add(CStr(X))
+            counter = counter + 1
+
+        Loop
 
     End Sub
 End Class
